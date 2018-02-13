56c0bc7303220f7edecf61da136f918934a51026
Tseko Tsolov <tsekotsolov@gmail.com>
2018-02-13 15:37:10 +0200

    up

---
+++ 01.DOM/Lab/Part 2/P01_ListItems/script.js
@@ -0,0 +1,11 @@
+function addItem() {
+
+    let userInput = document.getElementById('newItemText').value;
+    if (userInput != '') {
+        let listItem = document.createElement('li');
+        listItem.textContent = userInput;
+        document.getElementById('items').appendChild(listItem);
+        document.getElementById('newItemText').value = ' ';
+    }
+
+}
