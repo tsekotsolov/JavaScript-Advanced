529861d24dea88bdcb4dbdc4f7f075d6261e898e
tsekotsolov <tsekotsolov@gmail.com>
2018-02-14 19:38:36 +0200

    up

---
+++ 01.DOM/Lab/Part 2/P05_Gradient/script.js
@@ -0,0 +1,19 @@
+function attachGradientEvents() {
+
+    let box = document.getElementById('gradient');
+    box.addEventListener('mousemove', printCoordinates)
+    box.addEventListener('mouseout', boxOut)
+
+
+    function printCoordinates(event) {
+        let xPosition = e.offsetX;
+        let totalWidth = this.clientWidth - 1;
+        let percentage = Math.trunc(xPosition / totalWidth * 100);
+        document.getElementById('result').textContent = percentage + '%';
+    }
+
+    function boxOut(event) {
+        document.getElementById('result').textContent = ''
+    }
+
+}
