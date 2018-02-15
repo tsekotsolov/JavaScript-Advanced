ab61a24380623d91069b74e4784033e14dd874af
tsekotsolov <tsekotsolov@gmail.com>
2018-02-15 21:12:52 +0200

    up

---
+++ 01.DOM/Exercises/P05_Notification/script.js
@@ -0,0 +1,9 @@
+function notify(message) {
+  let notificationDiv = document.getElementById('notification');
+
+  notificationDiv.textContent = message;
+  notificationDiv.style.display = "block"
+
+  setTimeout(()=> notificationDiv.style.display = "none", 2000);
+
+}
