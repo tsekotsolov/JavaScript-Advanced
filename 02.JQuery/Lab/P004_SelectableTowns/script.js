ea604feec699edfc5107b2ff0e9079c8ea8efcb3
tsekotsolov <tsekotsolov@gmail.com>
2018-02-18 17:33:25 +0200

    up

---
+++ 02.JQuery/Lab/P004_SelectableTowns/script.js
@@ -0,0 +1,26 @@
+function attachEvents() {
+  $('#items').find('li').click(select);
+  $('#showTownsButton').click(printTowns);
+
+  function select() {
+
+    if ($(this).attr('data-selected')) {
+      $(this).css('background-color', '')
+      $(this).removeAttr('data-selected');
+
+    } else {
+      $(this).css('background-color', '#ddd');
+      $(this).attr('data-selected', true);
+    }
+
+  }
+
+  function printTowns() {
+    
+    let selectedTowns = $('li[data-selected]').toArray().map(li=>li.textContent).join(', ');
+    
+    $('#selectedTowns').text(`Selected towns: ${selectedTowns}`)
+
+  }
+
+}
