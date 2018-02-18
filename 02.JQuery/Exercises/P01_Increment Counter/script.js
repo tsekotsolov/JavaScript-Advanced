143211c11ca8f358b7992faa9796b49096a39894
tsekotsolov <tsekotsolov@gmail.com>
2018-02-18 21:56:01 +0200

    up

---
+++ 02.JQuery/Exercises/P01_Increment Counter/script.js
@@ -0,0 +1,9 @@
+function increment(selector) {
+
+    let container = $('selector');
+    let fragment = document.createDocumentFragment();
+    let textarea = $('<textarea>');
+
+    console.log(container);
+    console.log(fragment);
+}
