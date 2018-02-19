e0beefcb69225ecde2931e2c44fb0c93b9d8770a
tsekotsolov <tsekotsolov@gmail.com>
2018-02-19 14:36:40 +0200

    up

--- 02.JQuery/Exercises/P04_FormValidation/formValidation.js
+++ 02.JQuery/Exercises/P04_FormValidation/formValidation.js
@@ -1,3 +1,46 @@
 function validate() {
-    // TODO
+
+
+let username = $('#username');
+let email = $('#email');
+let password =  $('#password');
+let confirmPass = $('#confirm-password');
+let submitBtn = $('#submit');
+let companyCheckbox = $('#company')
+let companyNumber = $('#companyNumber');
+let compayInfo = $('#companyInfo')
+let validDiv = $('#valid');
+   
+    
+    submitBtn.click((event)=>{
+        event.preventDefault();
+        validateForm();
+    }) 
+
+    companyCheckbox.change(()=>{
+        if (companyCheckbox.is(':checked')) {
+            compayInfo.css('display','block');
+        }
+        else{
+            compayInfo.css('display','none');
+        }
+    })
+    
+   
+    function validateForm(){
+        const regexUserName = /^[0-9A-Za-z]{3,20}$/gm;
+        validateInput(regexUserName,username)
+        
+    }
+
+    function validateInput(regexPattern,input){
+        if (regexPattern.test(input.val())) {
+           input.css('border','none')
+        }
+        else{
+            input.css('border','2px solid red')
+        }
+    }
+
+
 }
