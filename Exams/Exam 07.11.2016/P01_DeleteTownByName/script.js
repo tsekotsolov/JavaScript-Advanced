f371a9f4d541ee8ea2c6c2840807fd5810f46edc
tsekotsolov <tsekotsolov@gmail.com>
2018-02-20 16:47:11 +0200

    кз

--- Exams/Exam 07.11.2016/P01_DeleteTownByName/script.js
+++ Exams/Exam 07.11.2016/P01_DeleteTownByName/script.js
@@ -1,3 +1,10 @@
 function attachEvents() {
- 
+  function attachEvents() {
+    let options = $('option');
+   
+    options.each((iterator,option)=>{
+   
+     console.log(option.value);
+    });
+   }
 }
