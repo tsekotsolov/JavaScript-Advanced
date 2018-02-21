fe781f8925c301fb6dc9c0eb236af96b84a7b048
tsekotsolov <tsekotsolov@gmail.com>
2018-02-21 17:17:34 +0200

    up

--- 02.JQuery/Exercises/P04_FormValidation/formValidation.js
+++ 02.JQuery/Exercises/P04_FormValidation/formValidation.js
@@ -35,7 +35,7 @@ function validate() {
         validateInput(regexPassword, confirmPass);
 
         if (password.val() != confirmPass.val()) {
-            
+            //password.css('border', '2px solid red');
             confirmPass.css('border', '2px solid red');
         }
 
@@ -52,6 +52,7 @@ function validate() {
             if ($(input).attr("style")) {
 
                 isValid = false;
+               
 
             }
         });
