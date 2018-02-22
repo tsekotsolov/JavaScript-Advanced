fa67af2075cb2e1ba93402bf370f6bb4ed01012c
tsekotsolov <tsekotsolov@gmail.com>
2018-02-22 13:29:16 +0200

    up

---
+++ 03.Advanced functions/Exercises/P01_SortArray.js
@@ -0,0 +1,15 @@
+function sort(array,order){
+
+if (order === "asc") {
+  console.log(array.sort((a,b)=>a-b));
+  
+}
+
+else{
+  console.log(array.sort((a,b)=>b-a));
+}
+
+}
+
+sort([14, 7, 17, 6, 8], 'asc');
+sort([14, 7, 17, 6, 8], 'desc');
