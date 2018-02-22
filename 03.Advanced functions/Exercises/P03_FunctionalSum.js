cff0f46b8c31e974163ccf60d81c29aa909bb342
Tseko Tsolov <tsekotsolov@gmail.com>
2018-02-22 19:42:09 +0200

    up

--- 03.Advanced functions/Exercises/P03_FunctionalSum.js
+++ 03.Advanced functions/Exercises/P03_FunctionalSum.js
@@ -1,6 +1,6 @@
 (function solve() {
     let sum = 0;
-    
+
     function add(value) {
         sum += value;
         return add;
@@ -8,5 +8,6 @@
     add.toString= function () {
         return sum;
     }
+
     return add;
-})()
+})();
