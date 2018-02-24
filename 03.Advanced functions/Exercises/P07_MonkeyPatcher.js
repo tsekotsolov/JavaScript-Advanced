b80d73f430e088a852bf8e0e92767690ec038d78
tsekotsolov <tsekotsolov@gmail.com>
2018-02-24 16:22:01 +0200

    up

---
+++ 03.Advanced functions/Exercises/P07_MonkeyPatcher.js
@@ -0,0 +1,74 @@
+function result(command) {
+
+  let commands = {
+    upvote: () => {
+      this.upvotes++;
+    },
+
+    downvote: () => {
+      this.downvotes++;
+    },
+
+    score: () => {
+
+      let scoreArr = [];
+      if (this.upvotes + this.downvotes > 50) {
+
+        let addedNumber = Math.ceil(Math.max(this.upvotes, this.downvotes) * 0.25);
+        scoreArr.push(`${this.upvotes+addedNumber}`);
+        scoreArr.push(`${this.downvotes+addedNumber}`);
+        scoreArr.push(`${this.upvotes}` - `${this.downvotes}`);
+
+      } else {
+        scoreArr.push(`${this.upvotes}`);
+        scoreArr.push(`${this.downvotes}`);
+        scoreArr.push(`${this.upvotes}` - `${this.downvotes}`);
+      }
+
+      scoreArr = scoreArr.map(Number);
+
+      let rating = '';
+      if (this.upvotes + this.downvotes < 10) {
+        rating = 'new';
+        scoreArr.push(rating);
+      } else if (this.upvotes / (this.upvotes + this.downvotes) > 0.66) {
+        rating = 'hot';
+        scoreArr.push(rating);
+      } else if ((this.upvotes / (this.upvotes + this.downvotes) <= 0.66) && (this.upvotes > 100 || this.downvotes > 100) && this.upvotes - this.downvotes >= 0) {
+        rating = 'controversial';
+        scoreArr.push(rating);
+      } else if (this.upvotes - this.downvotes < 0) {
+        rating = 'unpopular';
+        scoreArr.push(rating);
+      } else {
+        rating = 'new';
+        scoreArr.push(rating);
+      }
+
+      return scoreArr;
+    }
+  }
+  return commands[command]();
+}
+
+
+var forumPost = {
+  id: '1234',
+  author: 'author name',
+  content: 'these fields are irrelevant',
+  upvotes: 4,
+  downvotes: 5
+};
+
+// Under border case
+var answer = result.call(forumPost, 'score');
+var expected = [4, 5, -1, 'new'];
+
+
+result.call(forumPost, 'downvote');
+answer = result.call(forumPost, 'score');
+
+result.call(forumPost, 'upvote');
+result.call(forumPost, 'upvote');
+answer = result.call(forumPost, 'score');
+console.log((answer));
