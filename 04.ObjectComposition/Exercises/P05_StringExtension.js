d402cbf9753bdb1d1a58ab4ac6cb1c0c38b4026e
Tseko Tsolov <tsekotsolov@gmail.com>
2018-02-27 16:00:12 +0200

    up

--- 04.ObjectComposition/Exercises/P05_StringExtension.js
+++ 04.ObjectComposition/Exercises/P05_StringExtension.js
@@ -1,23 +1,24 @@
 (function () {
 
+
   String.prototype.ensureStart = function (str) {
 
-    if (!this.includes(str)) {
-      return result = str + this;
+    if (!this.toString().startsWith(str)) {
+      return result = str + this.toString();
     }
-    return this.valueOf(this);
+    return this.toString();
   }
 
   String.prototype.ensureEnd = function (str) {
 
-    if (!this.includes(str)) {
-      return result = this + str;
+    if (!this.toString().endsWith(str)) {
+      return result = this.toString() + str;
     }
-    return this.valueOf(this);
+    return this.toString();
 
   }
   String.prototype.isEmpty = function () {
-    if (this.valueOf(this) === '') {
+    if (this.toString() === '') {
       return true;
     }
     return false;
@@ -26,43 +27,66 @@
   String.prototype.truncate = function (n) {
 
     let currentString = this.toString()
-    if (n > currentString.length) {
+    if (n > currentString.length-1) {
       return currentString;
-    } else {
-
-      if (n < 4) {
-        return '.'.repeat(n)
-      }
-
-      while (currentString.lastIndexOf(' ') != -1) {
-        if (currentString.lastIndexOf(' ') <= n + 3) {
-          return currentString.slice(0, currentString.lastIndexOf(' ')) + '...';
-        }
-        currentString = currentString.slice(0, currentString.lastIndexOf(' '))
-      }
-      return currentString.slice(0, n - 3) + '...'
     }
+
+    if (n < 4) {
+      return '.'.repeat(n)
+    }
+
+    while (currentString.lastIndexOf(' ') != -1) {
+      if (currentString.lastIndexOf(' ') <= n - 2) {
+        return currentString.slice(0, currentString.lastIndexOf(' ')) + '...';
+      }
+      currentString = currentString.slice(0, currentString.lastIndexOf(' '))
+    }
+    return currentString.slice(0, n - 3) + '...'
+  }
+
+  String.format = function (string, ...arguments) {
+    for (let i = 0; i < arguments.length; i++) {
+      let index = string.indexOf("{" + i + "}");
+      while (index != -1) {
+        string = string.replace("{" + i + "}", arguments[i]);
+        index = string.indexOf("{" + i + "}");
+      }
+    }
+    return string;
   }
 
 })();
 
 
-let str = 'my string';
+// let str = 'my string';
 
-str = (str.ensureStart('my'));
-console.log(str);
+// str = (str.ensureStart('my'));
+// console.log(str);
 
-str = (str.ensureStart('hello '));
-console.log(str);
+// str = (str.ensureStart('hello '));
+// console.log(str);
 
-str = str.truncate(16);
-console.log(str);
+// str = str.truncate(16);
+// console.log(str);
 
-str = str.truncate(14);
-console.log(str);
-str = str.truncate(8);
-console.log(str);
-str = str.truncate(4);
-console.log(str);
-str = str.truncate(2);
+// str = str.truncate(14);
+// console.log(str);
+// str = str.truncate(8);
+// console.log(str);
+// str = str.truncate(4);
+// console.log(str);
+// str = str.truncate(2);
+// console.log(str);
+
+// str = String.format('The {0} {1} fox', 'quick', 'brown');
+// console.log(str);
+// str = String.format('jumps {0} {1}',
+//   'dog');
+// console.log(str);
+
+str = 'the quick brown fox jumps over the lazy dog';
+// str = str.ensureStart('the ');
+
+//str= str.truncate(6);
+str = str.truncate(43);
 console.log(str);
