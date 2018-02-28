b8f97b475cf4230ba3dfa624a3d249e6294971e3
Tseko Tsolov <tsekotsolov@gmail.com>
2018-02-28 16:45:00 +0200

    up

---
+++ 05.UnitTesting/Lab/P02_PlayingCards.js
@@ -0,0 +1,36 @@
+function createCard(face, suit) {
+
+    let validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', ' Q', 'K', 'A'];
+
+    let validSuits = {
+        S: '\u2660',
+        H: '\u2665',
+        D: '\u2666',
+        C: '\u2663'
+    }
+
+    if (!validFaces.includes(face)) {
+
+        throw new Error("Invalid card face: " + face);
+    }
+
+    if (!validSuits.hasOwnProperty(suit)) {
+
+        throw new Error("Invalid card suit: " + suit);
+    }
+
+    let card = {
+        face,
+        suit,
+        toString: function () {
+            return face + validSuits[suit];
+        }
+    }
+
+    return card;
+
+}
+
+//createCard('2', 'H');
+
+console.log('' + createCard('2', 'H'));
