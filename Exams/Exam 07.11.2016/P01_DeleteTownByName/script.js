c8e982b870a49b81d0d745699d2ddf08e290d9cf
tsekotsolov <tsekotsolov@gmail.com>
2018-03-05 13:40:10 +0200

    up

--- Exams/Exam 07.11.2016/P01_DeleteTownByName/script.js
+++ Exams/Exam 07.11.2016/P01_DeleteTownByName/script.js
@@ -1,10 +1,24 @@
 function attachEvents() {
-  function attachEvents() {
-    let options = $('option');
-   
-    options.each((iterator,option)=>{
-   
-     console.log(option.value);
+
+  let options = $('option');
+  let button = $('#btnDelete');
+  let input = $('#townName');
+  let result = $('#result')
+
+  button.click(function () {
+    let removed = false;
+
+    options.each((iterator, option) => {
+
+      if (option.textContent === input.val()) {
+        option.remove();
+        removed = true;
+      }
+
     });
-   }
+
+    !removed ? result.text(`${input.val()} not found.`) : result.text(`${input.val()} deleted.`);
+
+    input.val('');
+  })
 }
