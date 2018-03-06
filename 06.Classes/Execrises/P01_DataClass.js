76c609e93d697269a603538ec48ce43e65aa5340
tsekotsolov <tsekotsolov@gmail.com>
2018-03-06 13:02:22 +0200

    up

---
+++ 06.Classes/Execrises/P01_DataClass.js
@@ -0,0 +1,12 @@
+class dataRequest {
+
+  constructor(method, uri, version, message) {
+    this.method = method;
+    this.uri = uri;
+    this.version = version;
+    this.message = message;
+    this.response = undefined;
+    this.fulfilled = false;
+
+  }
+}
