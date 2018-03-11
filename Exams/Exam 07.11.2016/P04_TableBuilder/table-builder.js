0bc0d3712776e22108c3fc4dbc27e6b76f6f659a
Tseko Tsolov <tsekotsolov@gmail.com>
2018-03-11 13:41:19 +0200

    up

--- Exams/Exam 07.11.2016/P04_TableBuilder/table-builder.js
+++ Exams/Exam 07.11.2016/P04_TableBuilder/table-builder.js
@@ -1,16 +1,15 @@
 function tableBuilder(selector) {
     let container = $(selector);
 
-    function createTable(columnNames){
+    function createTable(columnNames) {
 
         container.empty();
         let table = $('<table>');
         let tableRow = $('<tr>');
-        
+
         for (const column of columnNames) {
-            let tableHeader = $(`<th>`);
-            tableHeader.text(column);
-            tableRow.append(tableHeader)
+
+            $(`<th>`).text(column).appendTo(tableRow)
         }
 
         tableRow.append(`<th>Action</th>`)
@@ -18,27 +17,26 @@ function tableBuilder(selector) {
         container.append(table);
     }
 
-    function fillData(dataRows){
-     let table = $(`${selector} table`);
+    function fillData(dataRows) {
+        let table = $(`${selector} table`);
 
-     for (const dataRow of dataRows) {
-         let tableRow = $('<tr>')
-         for (const data of dataRow) {
-             let td = $('<td>');
-             td.text(data);
-             tableRow.append(td);
-         }
-         let delBtn  = $('<td><button>Delete</button></td>');
+        for (const dataRow of dataRows) {
+            let tableRow = $('<tr>')
+            for (const data of dataRow) {
 
-         delBtn.on('click',function(){
-             this.parentNode.remove();
-         })
-         tableRow.append(delBtn);
-         table.append(tableRow);
-     }
+                $('<td>').text(data).appendTo(tableRow)
+            }
+            let delBtn = $('<td><button>Delete</button></td>');
+
+            delBtn.on('click', function () {
+                this.parentNode.remove();
+            })
+            tableRow.append(delBtn);
+            table.append(tableRow);
+        }
     }
 
-    return{
+    return {
         createTable,
         fillData
     }
