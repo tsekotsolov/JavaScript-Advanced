0dea227cc98d67831268b1d5a47b9ae092c2abe2
Tseko Tsolov <tsekotsolov@gmail.com>
2018-03-15 21:01:04 +0200

    up

--- Exams/Exam 05.09.2017/P03_Repository/repository.js
+++ Exams/Exam 05.09.2017/P03_Repository/repository.js
@@ -7,7 +7,9 @@ class Repository {
     }
 
     get count() {
+
         return [...this.data].length;
+
     }
 
     set props(value) {
@@ -43,7 +45,7 @@ class Repository {
         }
 
         this.data.delete(id);
-       
+
     }
 
     validate(value) {
@@ -80,33 +82,33 @@ let entity = {
 };
 repository.add(entity); // Returns 0
 repository.add(entity); // Returns 1
-console.log(repository.get(0));
-// {"name":"Kiril","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
-console.log(repository.get(1));
-// {"name":"Kiril","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
-//Update an entity
-entity = {
-    name: 'Valio',
-    age: 19,
-    birthday: new Date(1998, 0, 7)
-};
-repository.update(1, entity);
-console.log(repository.get(1));
-// {"name":"Valio","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
-// Delete an entity
-repository.del(0);
-console.log(repository.count); // Returns 1
-let anotherEntity = {
-    name1: 'Nakov',
-    age: 26,
-    birthday: new Date(1991, 0, 21)
-};
-//repository.add(anotherEntity); // should throw an Error
-anotherEntity = {
-    name: 'Nakov',
-    age: 26,
-    birthday: 1991
-};
-//repository.add(anotherEntity); // should throw a TypeError
-repository.del(-1); // should throw Error for invalid id
 
+// console.log(repository.get(0));
+// // {"name":"Kiril","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
+// console.log(repository.get(1));
+// // {"name":"Kiril","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
+// //Update an entity
+// entity = {
+//     name: 'Valio',
+//     age: 19,
+//     birthday: new Date(1998, 0, 7)
+// };
+// repository.update(1, entity);
+// console.log(repository.get(1));
+// // {"name":"Valio","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
+// // Delete an entity
+// repository.del(0);
+// console.log(repository.count); // Returns 1
+// let anotherEntity = {
+//     name1: 'Nakov',
+//     age: 26,
+//     birthday: new Date(1991, 0, 21)
+// };
+// //repository.add(anotherEntity); // should throw an Error
+// anotherEntity = {
+//     name: 'Nakov',
+//     age: 26,
+//     birthday: 1991
+// };
+// //repository.add(anotherEntity); // should throw a TypeError
+// repository.del(-1); // should throw Error for invalid id
