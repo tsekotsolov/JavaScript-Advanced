cc818c41e621ff976dcb6e3d81d7322ffab36b32
tsekotsolov <tsekotsolov@gmail.com>
2018-03-16 15:50:27 +0200

    up

--- Exams/Exam 05.09.2017/P03_Repository/repository.js
+++ Exams/Exam 05.09.2017/P03_Repository/repository.js
@@ -28,6 +28,10 @@ class Repository {
 
     get(id) {
 
+        if (this.data.get(id) === undefined) {
+            throw new TypeError(`Entity with id: ${id} does not exist!`)
+        }
+
         return this.data.get(id);
     }
 
@@ -62,53 +66,13 @@ class Repository {
             }
         }
 
-        return true;
+        
     }
 }
 
-// Initialize props object
-let properties = {
-    name: "string",
-    age: "number",
-    birthday: "object"
+let props = {
+    color: "string",
+    length: "number"
 };
-//Initialize the repository
-let repository = new Repository(properties);
-// Add two entities
-let entity = {
-    name: "Kiril",
-    age: 19,
-    birthday: new Date(1998, 0, 7)
-};
-repository.add(entity); // Returns 0
-repository.add(entity); // Returns 1
-
-// console.log(repository.get(0));
-// // {"name":"Kiril","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
-// console.log(repository.get(1));
-// // {"name":"Kiril","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
-// //Update an entity
-// entity = {
-//     name: 'Valio',
-//     age: 19,
-//     birthday: new Date(1998, 0, 7)
-// };
-// repository.update(1, entity);
-// console.log(repository.get(1));
-// // {"name":"Valio","age":19,"birthday":"1998-01-06T22:00:00.000Z"}
-// // Delete an entity
-// repository.del(0);
-// console.log(repository.count); // Returns 1
-// let anotherEntity = {
-//     name1: 'Nakov',
-//     age: 26,
-//     birthday: new Date(1991, 0, 21)
-// };
-// //repository.add(anotherEntity); // should throw an Error
-// anotherEntity = {
-//     name: 'Nakov',
-//     age: 26,
-//     birthday: 1991
-// };
-// //repository.add(anotherEntity); // should throw a TypeError
-// repository.del(-1); // should throw Error for invalid id
+let repo = new Repository(props);
+repo.del(5);
